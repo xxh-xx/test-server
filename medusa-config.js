6c724fd66fb7e3a7145b7d13f83f4a14278d47e9
xxh66 <xxh668899@vip.qq.com>
2023-01-16 22:33:54 +0800

    no message

--- medusa-config.js
+++ medusa-config.js
@@ -52,6 +52,20 @@ const plugins = [
   //     webhook_secret: STRIPE_WEBHOOK_SECRET,
   //   },
   // },
+  {
+    resolve: `medusa-plugin-sendgrid`,
+    options: {
+      api_key: process.env.SENDGRID_API_KEY,
+      from: process.env.SENDGRID_FROM,
+      order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID,
+      localization: {
+        "de-DE": { // locale key
+          order_placed_template: 
+            process.env.SENDGRID_ORDER_PLACED_ID_LOCALIZED,
+        },
+      },
+    },
+  }
 ];
 
 module.exports = {
